0f7d3e758d2b3f28ccd46a744b6d89a2b27a3532
Jonatas Carvalho Sousa <jcarvals@everis.com>
2021-02-11 17:49:26 -0300

    feature-2: atualizações no root.

---
+++ src/pages/Promotion/Search/Form/Form.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const PagesPromotionForm = () => {
+    const { id } = useParams();
+
+    return(
+    <div>
+        FORM @
+        {id && <div>id: {id} teste</div>}
+        Alo
+    </div>
+    );
+}
+export default PagesPromotionForm;
